c6882b99a526574190b50508669398d59aca7b1e
Noah <nzinn7734@gmail.com>
2025-05-11 20:35:07 -0400

    Initial add

---
+++ src/main/java/com/example/space_events_notification/SpaceEventsNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.space_events_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceEventsNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceEventsNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/space_events_notification/SpaceEventsNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.space_events_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceEventsNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
